e5f9c26e91369322a8cfa7b4a94e0594b4c95e33
Henry <henryapanisile@gmail.com>
2025-09-21 04:22:36 +0100

    modified:   middleware/auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,14 +3,15 @@ import jwt from 'jsonwebtoken';
 const auth = (req, res, next) => {
     const token = req.header('Authorization')?.replace('Bearer ', '');
     if (!token) return res.status(401).json({ error: 'No token provided' });
+    return res.status(202).json({ token: token })
 
-    try {
+    /*try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
     } catch (error) {
         res.status(401).json({ error: 'Invalid token' });
-    }
+    }*/
 };
 
 export default auth;
