4a44989623628221b9a4d9721c72def5e6774214
Henry <henryapanisile@gmail.com>
2025-09-21 04:53:50 +0100

    modified:   middleware/auth.js 	modified:   routes/brands.js 	modified:   routes/users.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,8 @@
 import jwt from 'jsonwebtoken';
 
 const auth = (req, res, next) => {
-    const authHeader = req.header('Authorization');
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        return res.status(401).json({ error: 'No token provided' });
-    }
-    const token = authHeader.replace('Bearer ', '');
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+    if (!token) return res.status(401).json({ error: 'No token provided' });
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
